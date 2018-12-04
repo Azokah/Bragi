098aaff52d6fdb2e2e70c47a0abff843c77ef71b
Azokah <jaimegbonorino@gmail.com>
2018-12-03 22:01:36 -0300

    Front terminado

--- src/web/bragiweb/src/utils/Twitter.js
+++ src/web/bragiweb/src/utils/Twitter.js
@@ -1,29 +1,75 @@
 import React, { Component } from 'react'
 import {fetchApi} from "./restMethods.js"
 import '../App.css';
+import Parser from 'html-react-parser';
 
-//var config = require('../config/bragi.config.json');
-const logo = require("../static/images/tenor.gif");//No me deja usar una constante de config.bragiPortrait
+var config = require('../config/bragi.config.json');
 
-//import SideMenu from './utils/SideMenu.js'
+//const logo = require("../static/images/tenor.gif");//No me deja usar una constante de config.bragiPortrait
+
 
 class Twitter extends Component {
   constructor(props){
     super(props)
-	  this.state = {helloString: "Twitter"};
+    this.state = {helloString: "Twitter",text: "No records of tweets yet.", htmlText: "No records of tweets yet."};
+    
+    fetchApi("/twitter/get_timeline").then((result) => {
+      this.setState({text:result});
+      //this.setState({htmlText: this.createFeed()});
+      //console.log(this.state.htmlText);
+     });
   }
   
+  handleSubmit(event) {
+		event.preventDefault();
+		//CE5J60664
+		console.log("Mandando: ");
+		console.log(event.target[0].value);
+		console.log(event.target[1].value);
+
+		fetch(config.url_api + '/twitter/post_tweet', {
+			method: 'post',
+			headers: {
+				'Accept': 'text/plain',
+    			'Content-Type': 'text/plain'
+			  },
+			body: JSON.stringify({
+				mensaje: event.target[0].value,
+			  })
+		});
+	}
+
+  createFeed(){
+    var html = "";
+    console.log(this.state.text);
+		for (var i = 0; i < this.state.text.length; i++) {
+      html += "++++++++++++++++++++++++++++++++++++";
+      html +=  "<p>" + this.state.text[i].user +': ' + this.state.text[i].mensaje + "</p>";
+    }
+    return html;
+	}
+  
   render() {
-    return (
-      <div>
-        <header className="App-header">
-          <p> { this.state.helloString } </p>
-        </header>
-        <img src={logo} className="IntroLogo" alt="" />
-        <p> Under construction, come back soon!</p>
-      </div>
-    );
-  }
+		return (
+			<div>
+				<header className="App-header">
+					<p> Twitter</p>
+				</header>
+				<div>
+					<p/>
+					<form onSubmit={this.handleSubmit}>
+						<p /><label>Tweet:
+                		<input type="text" name="mensaje" className="MessageInputText" />
+						</label>
+						<p /><input type="submit" value="Submit" />
+					</form>
+				</div>
+        <div className="ScrollBox">
+          {Parser(this.createFeed())}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Twitter;
